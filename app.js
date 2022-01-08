c483ab93841efee76ce4e14771f383057323785f
Stefan Feldner <63292629+stefanfeldner@users.noreply.github.com>
2022-01-08 15:37:38 +0100

    Adding message logic

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
-const messageInput = document.getElementById("message");
+$("#message").keypress((e) => {
+  if (e.which === 13 && $("#message").val() !== '') {
+    // prevent script injections by encoding < and >
+    const newMessageText = $("#message").val().replace(/</g, "&lt;").replace(/>/g, "&gt;");
+    const time = new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+    const date = new Date().toLocaleDateString();
+    
+    console.log(time, date)
+    $("#message").val("");
 
-console.log(messageInput);
-
-$(messageInput).keypress((e) => {
-  if (e.which === 13) alert("Enter");
+    createMessage("right", newMessageText, time, date);
+  }
 });
+
+const createMessage = (side, text, time, date) => {
+  $("#chat-content").append(`
+    <div class="chat-message-wrapper ${side}">
+      <div class="chat-message">
+        <div class="chat-message-text">${text}</div>
+        <div class="chat-message-time">${time}</div>
+        <div class="chat-message-date">${date}</div>
+      </div>
+    </div>
+  `);
+};
+
+
